652f16f107028095a1759a82a1a453efa0dc9ae4
everydayday <96685431+everydayday@users.noreply.github.com>
2024-03-07 18:18:06 +0900

    3.7

---
+++ datastrucutre/src/chap2_기본자료구조/Test_고급과제_option_행렬을클래스로정의하여연산.java
@@ -0,0 +1,39 @@
+package chap2_기본자료구조;
+
+class Matrix{
+
+	   int cols,rows;
+
+	   int []data;
+	   
+	   public Matrix(int cols, int rows) {
+		// TODO Auto-generated constructor stub
+	}
+
+	  void inputMatrix(){
+
+	  }
+
+	  void showMatrix() {
+
+	  }
+
+	public class Test_고급과제_option_행렬을클래스로정의하여연산 {
+		
+		public static void main(String[] args) {
+			Matrix A = new Matrix(2,3);
+
+			A.inputMatrix();
+
+			Matrix C = A.addMatrix(B);
+			
+		}
+	}
+
+	 
+
+	    
+
+
+
+	
